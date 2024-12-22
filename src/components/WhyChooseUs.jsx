1b5465671e9ca064e8815c17cc01f2b7bed39062
ryuk <ryuk@ryuks-MacBook-Air.local>
2024-12-22 14:13:43 +0530

    v2.2.2

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -1,27 +1,109 @@
-import React from 'react';
-import './WhyChooseUs.css';
-import characterImage from '../assets/character_image.jpg';
+import React from "react";
+import "./WhyChooseUs.css";
+import video_editing_iamge from '../assets/services_images/video_editing.jpg';
+import customer_satifaction_image from '../assets/services_images/satisfaction.jpg';
+import consultancy_image from '../assets/services_images/consultancy.jpg';
+import desigining_image from '../assets/services_images/desigining.jpg';
 
 const WhyChooseUs = () => {
   return (
-    <section className="why-choose-us" id='chooseus'>
-      <div className="content">
-        <h2 className="title">Why <span className="highlight">Choose</span> Us!</h2>
-        <p className="description">
-          We are dedicated to delivering exceptional, custom-tailored designs that reflect your brand's unique identity. Our team combines creativity, innovation, and attention to detail to produce high-quality logos, branding materials, and illustrations. We prioritize client satisfaction, ensuring a collaborative process from concept to completion.
-          <br /><br />
-          With a proven track record of helping brands stand out, we bring your vision to life. Choose us for reliability, expertise, and a design experience that exceeds expectations.
-        </p>
-        <p className="value-points">Take a look at our value addition points:</p>
-        <ul className="points-list">
-          <li>Customer Tailored Experience</li>
-          <li>Client-Centric Approach</li>
-          <li>High-Quality Work</li>
-          <li>Guaranteed Satisfaction</li>
-        </ul>
-      </div>
-      <div className="image-container">
-        <img src={characterImage} alt="Character" className="character-image" />
+    <section className="why-choose-us">
+      <h5 className="section-subtitle">WHY CHOOSE US</h5>
+      <h2 className="section-title">
+        Why <span className="why-highlight">Unknown Pixel Media</span> is The Right Choice for You
+      </h2>
+      <div className="features-container">
+        <div className="feature-card-container">
+          <div className="feature-card-sub-conatiner">
+            <div className="feature-card"
+            style=
+            {
+              {
+                background:`linear-gradient(to top,rgb(0, 0, 0),rgba(255, 255, 255, 0)),url(${consultancy_image})`,
+                backgroundSize:'cover',
+                backgroundPosition:'center',
+                backgroundRepeat:'no-repeat'
+              }
+              }
+            >
+            <div className="feature-info">
+              <div className="icon"></div>
+              <h3>Consultancy</h3>
+              <p>
+                Learn from top industry professionals who bring years of real-world
+                experience to the classroom, providing you with the latest tools,
+                techniques, and insights needed to excel in your field.
+              </p>
+              </div>
+            </div>
+            <div className="feature-card" 
+            style=
+            {
+              {
+                background:`linear-gradient(to top,rgb(0, 0, 0),rgba(255, 255, 255, 0)),url(${video_editing_iamge})`,
+                backgroundSize:'cover',
+                backgroundPosition:'center',
+                backgroundRepeat:'no-repeat'
+              }
+              }>
+              <div className="feature-info">
+              <div className="icon"></div>
+              <h3>Video Editing</h3>
+              <p>
+                Earn certifications that are highly regarded by employers, helping
+                you to enhance your resume, gain industry recognition, and open
+                doors to new career opportunities.
+              </p>
+              </div>
+            </div>
+          </div>
+          <div className="feature-card"
+          style=
+          {
+            {
+              background:`linear-gradient(to top,rgb(0, 0, 0),rgba(255, 255, 255, 0)),url(${customer_satifaction_image})`,
+              backgroundSize:'cover',
+              backgroundPosition:'center',
+              backgroundRepeat:'no-repeat'
+            }
+            }
+          >
+          <div className="feature-info">
+            <div className="icon"></div>
+            <h3>100+ Customer Satisfaction</h3>
+            <p>
+              expert.io offers over 100 courses that cover essential skills in
+              today’s tech industry. Whether you're a beginner or an experienced
+              professional, our courses in web development, data science, and
+              cybersecurity provide practical, hands-on learning.
+            </p>
+          </div>
+          </div>
+        </div>
+        <div className="feature-card-container">
+          <div className="feature-card special-card"
+          style=
+          {
+            {
+              background:`linear-gradient(to top,rgb(0, 0, 0),rgba(255, 255, 255, 0)),url(${desigining_image})`,
+              backgroundSize:'cover',
+              backgroundPosition:'center',
+              backgroundRepeat:'no-repeat'
+            }
+            }
+          >
+          <div className="feature-info">
+            <div className="icon"></div>
+            <h3>Flexible Learning Schedules</h3>
+            <p>
+              At expert.io, we understand the importance of balancing learning
+              with a busy lifestyle. That’s why our courses are available
+              on-demand, allowing you to learn at your own pace, anytime and
+              anywhere.
+            </p>
+          </div>
+          </div>
+        </div>
       </div>
     </section>
   );
