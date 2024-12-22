b6df7f3e3778b05411043fb6e50b95090aaa89a5
ryuk <ryuk@ryuks-MacBook-Air.local>
2024-12-23 02:45:07 +0530

    [new Changes]

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -6,10 +6,18 @@ const AboutUs = () => {
   return (
     <section className="about-us">
       <div className="about-us-text">
-        <h2 className="about-us-headline">Serving Across the Globe</h2>
-        <p className="about-us-description">
-          With a strong presence in numerous countries, we are committed to delivering top-notch services worldwide, ensuring excellence in every corner of the world.
-        </p>
+        <h2 className="about-us-headline">About Us<br/>Serving Across the Globe</h2>
+        <p className="about-us-description"> 
+          As a global leader in digital media marketing, 
+          we specialize in delivering exceptional services across media strategy, 
+          consultancy, and marketing solutions. With an extensive presence in multiple
+           countries, we are dedicated to helping businesses thrive in the digital age. 
+           From crafting innovative campaigns to providing strategic guidance, our 
+           team ensures every client achieves unparalleled success.
+            Our mission is to bring your brand closer to its audience, 
+            leveraging cutting-edge technology and creative expertise 
+            to make a lasting impact worldwide. 
+            </p>
       </div>
       <div className='about-us-3d-model'>
     <GlobeComponent/>
