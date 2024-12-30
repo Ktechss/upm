948bd99d672c59caa6a23d57a6d3caf218afa5ea
ryuk <ryuk@ryuks-MacBook-Air.local>
2024-12-30 22:17:23 +0530

    [removed scrollbard]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 const Header = ({ logo, navigationLinks, socialIcons }) => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // state to toggle the menu
 
   const handleNavigation = (event, link) => {
     if (link.href.startsWith('#')) {
@@ -25,12 +26,24 @@ const Header = ({ logo, navigationLinks, socialIcons }) => {
     }
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="header-container">
       <div className="logo">
         <img src={logo} className="upm_logo" alt="UPM Logo" />
       </div>
-      <nav className="navigation">
+      
+      {/* Hamburger Menu Icon */}
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <nav className={`navigation ${isMenuOpen ? 'active' : ''}`}>
         <ul>
           {navigationLinks.map((link, index) => (
             <li key={index} className='nav-buttons'>
@@ -46,6 +59,7 @@ const Header = ({ logo, navigationLinks, socialIcons }) => {
           ))}
         </ul>
       </nav>
+
       <div className="social-icons">
         {socialIcons.map((icon, index) => (
           <a key={index} href={icon.href} aria-label={icon.label}>
