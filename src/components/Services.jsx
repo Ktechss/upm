f21fd39f52ce2af70553fd3b00e5b6e25c4d0532
Kartik Naik <kartiknaik@Kartiks-MacBook-Air.local>
2025-01-01 19:30:03 +0530

    [Lamp Light added]

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Services.css';
 import { StickyScroll } from './ui/StickyScroll';
+import { LampDemo } from './ui/LampContainer';
 import content from '../User_Data/Media_data';
 
 const Services = () => {
   return (
     <section className="services-section" id="services">
+      <div className='LampLight'>
+        <LampDemo/>
+        </div>
+      
       <StickyScroll content={content} />
     </section>
   );
